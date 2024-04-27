bc3ce2d7f0563db6688510040d59bcde001300c8
Micky Reed <xcal18r@gmail.com>
2024-04-27 23:34:09 +0800

    updated title screen with fade transition  -semi working goes to level 1

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -10,8 +10,8 @@ public class LevelLoader : MonoBehaviour
 
     private bool isTransitioning = false;
 
-    public string nextSceneName; // Name of the scene to load next
-    private string sceneToLoad; // Field to store the scene name to load
+    public string nextSceneName; // Index of the scene to load next
+    public string sceneToLoad; // Field to store the scene index to load
 
     void Start()
     {
@@ -21,37 +21,40 @@ public class LevelLoader : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("Update function is called"); // Add this line for debugging
+        Debug.Log("Update function is called "+ nextSceneName + sceneToLoad); // Add this line for debugging
         // Check for user input to trigger transition
-        if (!isTransitioning && Input.anyKeyDown || Input.GetMouseButtonDown(0))
+        if ((!isTransitioning) && Input.anyKeyDown || Input.GetMouseButtonDown(0))
         {
+            Debug.Log("**************** Inside recieved input **********************");
             isTransitioning = true;
             // Start the cross fade animation
             PlayFadeAnimation("CrossFade_End");
             // Load the level after the fade out
-            Invoke("LoadNextScene", fadeDuration);
+            //Invoke(nameof(LoadNextScene), fadeDuration);
+            SceneManager.LoadScene(1);
         }
+
     }
 
     void PlayFadeAnimation(string animationName)
     {
-        Debug.Log("UPlay Fade Animation is called");
+        Debug.Log("Play Fade Animation is called");
         // Play the specified animation
-        crossFadeCanvasGroup.GetComponent<Animation>().Play(animationName);
+        //crossFadeCanvasGroup.GetComponent<Animation>().Play(animationName);
     }
 
     //void LoadNextScene()
     //{
-    //    // Load the level_1 scene
-    //    //SceneManager.LoadScene("Level_1");
+    //    Load the level_1 scene
+    //    SceneManager.LoadScene("Level_1");
     //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     //}
 
     void LoadNextScene()
     {
         Debug.Log("Load Next Scene is called");
-        // Load the next scene using the provided scene name
-        SceneManager.LoadScene(nextSceneName);
+        //Load the next scene using the provided scene name
+        LoadSceneByName(nextSceneName);
     }
 
     // Function to allow loading of any scene by name
@@ -74,7 +77,10 @@ public class LevelLoader : MonoBehaviour
     {
         Debug.Log("Load Scene By NameDelayed");
         // Load the scene with the specified name
+        //SceneManager.LoadScene(sceneToLoad);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         SceneManager.LoadScene(sceneToLoad);
+
     }
 
 }
